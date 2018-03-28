3767f199c0ebb907c7548b9e5a7e68dbfddd6bc5
Amethyst <oconnellamethyst@gmail.com>
2018-03-28 14:25:48 -0500

    Facebookinator

---
+++ facebook.sikuli/facebook.py
@@ -0,0 +1,15 @@
+type(Key.F1, KeyModifier.ALT)
+
+click("1522263490344.png")
+wait("1522264234018.png")
+click("1522263518229.png")
+wait("1522264336267.png")
+type("facebook.com\n")
+wait("1522264388286.png")
+wait("1522264534418.png")
+click("1522263590885.png")
+type("No hands! #sikuli")
+wait("1522263681739.png")
+click("1522263681739.png")
+
+
