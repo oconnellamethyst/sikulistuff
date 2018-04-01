4d521dea4d02d660fb6b3a48937217959681905a
Amethyst <oconnellamethyst@gmail.com>
2018-04-01 12:59:59 -0500

    Take care of Tumblr horse code

---
+++ horse.sikuli/horse.py
@@ -0,0 +1,8 @@
+# Horse Babysitter
+# Takes care of Tumblr April Fools Horse while away at Easter
+while exists("1522602565761.png"):
+    if exists(Pattern("poop.png").exact()):
+        click(Pattern("poop.png").exact())
+    else:
+        pass
+            
